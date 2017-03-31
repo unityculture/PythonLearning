7850da750c0fa9a0afb64ab3ae2700f169c4ff0a
unityculture <unityculture@hotmail.com.tw>
2017-03-31 17:09:53 +0800

    first commit

---
+++ learning1.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+"""
+learning 
+"""
+
+x = 3
+print  x   
+x += 2
+print x
+x -= 1
+print x
+
+x,y=99,3    
+print x,y
+
+my_list = []
+my_list.append(1) 
+my_list.append(2) 
+my_list2 = [55.55,"Hi",3,99,222,222]
+my_list2[0]=333.333
+
+print len(my_list),sum(my_list),my_list2.count(222)
+print my_list2[1]
+print my_list2[0],my_list[1:3],my_list2[2:]
+
+
---
+++ learning2.py
@@ -0,0 +1,10 @@
+    passwd={'Mars':00000,'Mark':56680}
+passwd['Happy']=9999     
+passwd['Smile']=123456
+
+del passwd['Mars']
+passwd['Mark']=passwd['Mark']+1
+
+print passwd
+print passwd.keys()
+print passwd.get('Tony')
